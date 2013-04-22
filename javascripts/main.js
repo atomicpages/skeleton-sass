33f7a87a5af80dbdb4b7f69823ff38294abac51b
atomicpages <atomicpages@gmail.com>
2013-04-21 19:33:52 -0700

    General updates to pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 			$('body').addClass('fluid');
 			$("#mmf").text("Make me Fixed!");
 			$("#width_control").attr('type', 'number');
+			$('#stylesheet').attr('href', 'stylesheets/skeleton-fluid.css');
 			$('#width_control').change(function() {
 				$('.container').width( $(this).val() + "%" );
 			});
@@ -16,8 +17,35 @@ $(document).ready(function() {
 			$("#mmf").text("Make me Fluid!");
 			$("#width_control").val(92);
 			$("#width_control").attr('type', 'hidden');
+			$('#stylesheet').attr('href', 'stylesheets/skeleton.css');
 			$('.container').removeAttr('style');
 		}
+	});
+
+	$('#twelve').click(function() {
+		$.get('demo-12.html', function(data) {
+			$('#content').html(data);
+		});
+		if( $('body').hasClass('fluid') ) {
+			$("#mmf").text("Make me Fluid!");
+			$("#width_control").attr('type', 'hidden');
+			$("#width_control").val(92);
+			$('.container').removeAttr('style');
+		}
+		$('body').addClass('fixed twelve');
+		$('#stylesheet').attr('href', 'stylesheets/skeleton-twelve.css');
+	});
+
+	$('#twenty').click(function() {
+		$.get('demo-20.html', function(data) {
+			$('#content').html(data);
+		});
+		$('body').addClass('fixed twenty');
+		$('#stylesheet').attr('href', 'stylesheets/skeleton-twenty.css');
+	});
+
+	$("#reset").click(function() {
+		location.reload();
 	});
 
 	$(window).resize(function() {
